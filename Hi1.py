8933362c1fe52c40c466d1a2afd68442f06f34f7
ne8o <xmaladzegio@gmail.com>
2023-03-08 06:03:01 -0800

    commit

---
+++ Hi1.py
@@ -0,0 +1,13 @@
+'''
+მოთხოვნები:
+დარწმუნდი რომ input, კონკატენაცია და output ცალ-ცალკე გაქვს.
+
+მგალითი:
+What is your name? Oto
+Hello, Oto, nice to meet you!
+'''
+#input and output concatentions separately
+name = input("What is you name? ").title().strip()
+print("Hello,", name, "nice to meet you!")
+
+
